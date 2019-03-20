72ef832cea79dc67553235d6abb340e427da70bf
arthur38 <arthur.bonnel38@gmail.com>
2019-03-20 15:40:30 +0100

    Liaison de l'application angular à l'application firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-book-library';
+
+  constructor() {
+    // Initialize Firebase
+    const config = {
+        apiKey: 'AIzaSyBtdxrhzlk9TytkcQkaCXzlLMRwNLF6pTA',
+        authDomain: 'mybooklibrary-c1a3a.firebaseapp.com',
+        databaseURL: 'https://mybooklibrary-c1a3a.firebaseio.com',
+        projectId: 'mybooklibrary-c1a3a',
+        storageBucket: 'mybooklibrary-c1a3a.appspot.com',
+        messagingSenderId: '1086595216653'
+      };
+    firebase.initializeApp(config);
+    }
 }
