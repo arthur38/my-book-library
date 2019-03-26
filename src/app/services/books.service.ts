9d8ce00aa56ae35e8d4c29ee88b25b034a45db3c
arthur38 <arthur.bonnel38@gmail.com>
2019-03-26 13:51:59 +0100

    Création des methodes liées au service ( ajout, suppresion d'un livre, etc..)

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Book } from '../models/book.model';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
+
   constructor() { }
+
+  emitSubject() {
+    this.booksSubject.next(this.books);
+  }
+
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
+
+  getBooks() {
+    firebase.database().ref('/books')
+    .on('value', (data) => {
+      this.books = data.val() ? data.val() : [];
+      this.emitSubject()
+    });
+  }
+
+  getSingleBooks(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books/' + id).once('value').then(
+          (data) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  createNewBooks(newBook: Book) {
+    this.books.push(newBook);
+    this.saveBooks();
+    this.emitSubject();
+  }
+
+  removeBook(book: Book) {
+    const bookIndexToRemove = this.books.findIndex(
+      (bookEl) => {
+        if (bookEl === book) {
+          return true;
+        }
+      }
+    );
+    this.books.splice(bookIndexToRemove, 1);
+    this.saveBooks();
+    this.emitSubject();
+  }
 }
